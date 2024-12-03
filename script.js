c910f1b9c5afa84a72bf91e69da0a100910ff611
TuongDii1107 <156698116+TuongDii1107@users.noreply.github.com>
2024-12-03 08:55:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-// Hàm để thay đổi loại trường mật khẩu giữa 'password' và 'text'
-function togglePassword() {
-    // Lấy phần tử input mật khẩu
-    var passwordField = document.getElementById("password");
-    
-    // Kiểm tra xem trường mật khẩu hiện tại có phải là 'password' không
-    if (passwordField.type === "password") {
-        // Nếu là 'password', thay đổi thành 'text' để hiển thị mật khẩu
-        passwordField.type = "text";
-    } else {
-        // Nếu không, thay đổi lại thành 'password' để ẩn mật khẩu
-        passwordField.type = "password";
-    }
-}
+document.querySelectorAll('.thumbnail-images img').forEach((thumbnail) => {
+  thumbnail.addEventListener('click', (e) => {
+    document.querySelector('.product-image img').src = e.target.src;
+    document.querySelectorAll('.thumbnail-images img').forEach(img => img.classList.remove('active'));
+    e.target.classList.add('active');
+  });
+});
